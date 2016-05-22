c6bf237c46a6b51caccd506a9e7a992a9e570b19
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2016-05-22 19:33:56 -0400

    sign in with firebase

--- app/src/main/java/org/romeo/breadcrumbs/LaunchActivity.java
+++ app/src/main/java/org/romeo/breadcrumbs/LaunchActivity.java
@@ -1,7 +1,12 @@
 package org.romeo.breadcrumbs;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,18 +21,54 @@ public class LaunchActivity extends AppCompatActivity {
     @BindView(R.id.login_password) EditText passwordInput;
     @BindView(R.id.login_submit_button) Button submitButton;
 
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authListener;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
         ButterKnife.bind(this);
+
+        auth = FirebaseAuth.getInstance();
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Toast.makeText(LaunchActivity.this, "onAuthStateChanged:signed_in:" + user.getUid(), Toast.LENGTH_LONG).show();
+                } else {
+                    // User is signed out
+                    Toast.makeText(LaunchActivity.this, "onAuthStateChanged:signed_out", Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = emailInput.getText().toString();
                 String password = passwordInput.getText().toString();
-                Toast.makeText(LaunchActivity.this, email + " " + password, Toast.LENGTH_LONG).show();
+                auth.signInWithEmailAndPassword(email, password);
+//                Toast.makeText(LaunchActivity.this, email + " " + password, Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authListener != null) {
+            auth.removeAuthStateListener(authListener);
+        }
     }
 }
